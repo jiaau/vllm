1495f9a5ef8869a807024c1b8bccdae1644148ae
ao jia <ao_jia@outlook.com>
2025-08-25 03:56:51 -0700

    chore: add debug scripts

---
+++ offline_inference.py
@@ -0,0 +1,34 @@
+#  PYTHONPATH=/vllm-workspace/install/vllm python offline_inference.py
+import os
+from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer
+
+def main():
+    path = os.path.expanduser("/data/models/Qwen3-0.6B/")
+    tokenizer = AutoTokenizer.from_pretrained(path)
+    llm = LLM(path, enforce_eager=True, tensor_parallel_size=4)
+
+    sampling_params = SamplingParams(temperature=0.6, max_tokens=256)
+    prompts = [
+        "introduce yourself",
+        "list all prime numbers within 100",
+    ]
+    prompts = [
+        tokenizer.apply_chat_template(
+            [{"role": "user", "content": prompt}],
+            tokenize=False,
+            add_generation_prompt=True,
+            enable_thinking=True
+        )
+        for prompt in prompts
+    ]
+    outputs = llm.generate(prompts, sampling_params)
+
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+if __name__ == "__main__":
+    main()
